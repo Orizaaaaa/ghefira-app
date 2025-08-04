a61462e209c1b3a6802b61b48e20724bfb312a1b
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-08-04 20:02:22 +0700

    push

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -129,8 +129,6 @@ const Login = () => {
                     <ButtonPrimary typeButon={"submit"} className={`rounded-lg w-full mb-3 font-medium py-2 flex justify-center items-center`}>
                         {loading ? <Spinner className={`w-5 h-5`} size="sm" color="white" /> : 'Masuk'}
                     </ButtonPrimary>
-                    <p className='text-sm'>Belum punya akun ? <Link className='text-primary font-medium ' href={'/auth/register'} > Daftar</Link></p>
-
                 </form>
             </div>
 
