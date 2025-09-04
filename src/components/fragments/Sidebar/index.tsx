71a5cdc071553bfb8f228afac0bda0cc84b83469
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-09-05 03:38:05 +0700

    ss

--- src/components/fragments/Sidebar/index.tsx
+++ src/components/fragments/Sidebar/index.tsx
@@ -81,7 +81,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   const handleLogout = () => {
     localStorage.clear();
     document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
-    route.push("/auth/login")
+    route.push("/")
   }
 
 
